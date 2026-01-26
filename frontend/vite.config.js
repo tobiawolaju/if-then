0a6262980065b089617f81432fdfacb9020ebbf7
Tobiloba Awolaju <120692959+tobiawolaju@users.noreply.github.com>
2026-01-26 21:31:06 +0100

    :

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                dashboard: resolve(__dirname, 'dashboard.html'),
-            },
-        },
-    },
     server: {
         port: 5173,
         open: true
